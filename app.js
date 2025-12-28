d27f8afd62eeae44fe036841a44d576f042a7284
Satoshi <148063396+satoshii-ace@users.noreply.github.com>
2025-12-28 17:22:00 +0800

    major changes

--- app.js
+++
@@ -1,6 +0,0 @@
-const burger = ducument.querySelector('.burger');
-const nav = document.querySelector('.header');
-
-burger.addEventListener('click', () => {
-    nav.classList.toggle('navbar-active');
-});
